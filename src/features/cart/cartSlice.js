74870d1d0d8c88fb8b7a6b90f0fb34fd2996bdf8
JunShen <tony88010955@gmail.com>
2023-09-06 17:08:00 +0800

    first upload

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItem from "../../cartItems";
+
+const initialState = {
+  cartItems: cartItem,
+  amount: 2,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+
+// console.log(cartSlice);
+export const { clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
